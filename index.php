f656b27645e46e9c6e384a6f0c81681d5925fbbc
markconfig --global user.name <gardiolamark2003@gmail.com>
2025-11-11 11:43:28 +0800

    form updated

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <form action="PHPMailer.php" method="POST">
     <input type="text" name="name" placeholder="Your Name" required><br><br>
     <input type="email" name="email" placeholder="Recipient Email" required><br><br>
-    <button type="submit">Send Email</button>
+    <button type="submit">Test</button>
   </form>
 </body>
 </html>
